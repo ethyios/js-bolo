fd19426c0c9ce38a13060864245fd3254bb5afb8
ethyios <gugagom1998@gmail.com>
2020-07-04 17:07:50 -0300

    changes to organization of page
    . Switched angular logo with png exported svg
    . Made code a little more 'javascripted'

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 import './index.html'
 
+const campo_peso = document.getElementById("f-quant-bolo").innerText;
+const campo_preco = document.getElementById("f-preco-kg").innerText;
+
+function calcular() {
+    let int_peso = parseInt(campo_peso);
+    let float_preco = parseFloat(campo_preco);
+
+    let result = int_peso*float_preco/1000;
+    alert(result);
+}
+
+document.getElementById('calcular').onclick(calcular());
