0e45d79dec4079fa4c98e92d4fa2529621262d25
ethyios <gugagom1998@gmail.com>
2020-07-10 21:38:49 -0300

    correção de código depois de ver um tutorial no youtube
    A função calcular ainda não está totalmente funcional, mas está retornando resultados satisfatórios.

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-import './index.html'
-
-const campo_peso = document.getElementById("f-quant-bolo").innerText;
-const campo_preco = document.getElementById("f-preco-kg").innerText;
+const campo_peso = document.querySelector('.pesoBolo').textContent;
+const campo_preco = document.querySelector('.precoBolo').textContent;
+const subir = document.querySelector('.calcular');
+const resposta = document.querySelector('.resposta');
 
 function calcular() {
-    let int_peso = parseInt(campo_peso);
-    let float_preco = parseFloat(campo_preco);
+	let int_peso = parseInt(campo_peso);
+	let float_preco = parseFloat(campo_preco);
 
-    let result = int_peso*float_preco/1000;
-    alert(result);
+	let result = (int_peso * float_preco) / 1000;
+	console.log(result);
+	console.log(campo_peso);
+	console.log(campo_preco);
 }
 
-document.getElementById('calcular').onclick(calcular());
+subir.addEventListener('click', calcular);
